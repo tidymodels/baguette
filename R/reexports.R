d380cac0a36e0f039d931e98272c5b59c67d7aa6
topepo <mxkuhn@gmail.com>
2019-07-16 21:29:21 -0400

    added an importance extractor

--- R/reexports.R
+++ R/reexports.R
@@ -2,3 +2,7 @@
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+#' @importFrom generics var_imp
+#' @export
+generics::var_imp
---
+++ R/var_imp.R
@@ -0,0 +1,46 @@
+#' Obtain variable importance scores
+#'
+#' @param object An object.
+#' @param ... Not cuurently used.
+#' @return A tibble with columns for `term` (the predictor), `value` (the
+#'  importance score), and `used` (the occurances of the predictors).
+#' @details `baguette` can compute different variable importance scores for
+#'  each model in the ensemble. The `var_imp()` function returns the average
+#'  importance score for each model. Additionally, the function returns the
+#'  number of times that each predictor is included in the final prediction
+#'  equation.
+#'
+#' Specific methods used by the models are:
+#'
+#' _CART_: The model accumulates the improvement of the model that occurs when
+#'  a predictor is used in a split. These values are taken form the `rpart`
+#'  object. See `rpart::rpart.object()`.
+#'
+#' _MARS_: MARS models include a backwards elimination feature selection
+#'  routine that looks at reductions in the generalized cross-validation (GCV)
+#'  estimate of error. The `earth()` function tracks the changes in model
+#'  statistics, such as the GCV, for each predictor and accumulates the
+#'  reduction in the statistic when each predictor's feature is added to the
+#'  model. This total reduction is used as the variable importance measure. If a
+#'  predictor was never used in any of the MARS basis functions in the final
+#'  model (after pruning), it has an importance value of zero. `baguette` wraps
+#'  `earth::evimp()`.
+#'
+#' _Model Rules_: The underlying Cubist model calculates the occurrence of
+#'  predictors inside the model rule definitions as well as the linear
+#'  regression models contained within each rule. `baguette` wraps
+#'  `Cubist::summary.cubist()`.
+#'
+#' _C5.0_: `C5.0` measures predictor importance by determining the percentage
+#'  of training set samples that fall into all the terminal nodes after the
+#'  split. For example, the predictor in the first split automatically has an
+#'  importance measurement of 100 percent since all samples are affected by this
+#'  split. Other predictors may be used frequently in splits, but if the
+#'  terminal nodes cover only a handful of training set samples, the importance
+#'  scores may be close to zero.
+#' @export
+#' @export var_imp.bagger
+var_imp.bagger <- function(object, ...) {
+  object$imp
+}
+
