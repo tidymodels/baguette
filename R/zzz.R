cffbbb7ea141b11de5c1ef52ac4e258b903bc00f
‘topepo’ <mxkuhn@gmail.com>
2024-10-22 08:45:27 -0400

    use package doc file

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+
+# The functions below define the model information. These access the model
+# environment inside of parsnip so they have to be executed once parsnip has
+# been loaded.
+
+.onLoad <- function(libname, pkgname) {
+  # This defines model functions in the parsnip model database
+  make_bag_tree()
+  make_bag_mars()
+  make_bag_mlp()
+}
