f90631c421cc2d08460d09c65946e6945c1b4b10
topepo <mxkuhn@gmail.com>
2024-10-23 08:31:25 -0400

    nocov updates

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
+# nocov start
 
 # The functions below define the model information. These access the model
 # environment inside of parsnip so they have to be executed once parsnip has
@@ -9,3 +10,5 @@
   make_bag_mars()
   make_bag_mlp()
 }
+
+# nocov end
